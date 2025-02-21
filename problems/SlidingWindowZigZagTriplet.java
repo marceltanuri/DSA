c86f677cb3022e4fa7d9306f8e737d223e32b673
Marcel Tanuri <marcel.tanuri@gmail.com>
2025-02-21 00:25:34 -0300

    add new content

--- problems/SlidingWindowZigZagTriplet.java
+++ problems/SlidingWindowZigZagTriplet.java
@@ -5,15 +5,15 @@
 public class SlidingWindowZigZagTriplet {
 
     public int[] solution(int[] numbers) {
-        int slide = 3; // Trabalhando com triplas
-        if (numbers.length < slide) return new int[0]; // Retorna vazio se não houver espaço para uma tripla
+        int slide = 3; // Trabalhando com janelas
+        if (numbers.length < slide) return new int[0]; // Retorna vazio se não houver espaço para uma janela
     
         int[] result = new int[numbers.length - slide + 1];
     
         for (int i = 0; i < result.length; i++) { // Ajustando limite correto
             int balance = 0;
     
-            for (int j = 1; j < slide; j++) { // Processamos a tripla
+            for (int j = 1; j < slide; j++) { // Processamos a janela
                 if (numbers[i + j - 1] > numbers[i + j]) balance -= 1;
                 else if (numbers[i + j - 1] < numbers[i + j]) balance += 1;
                 else {
